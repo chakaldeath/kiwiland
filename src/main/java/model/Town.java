3cede8397afe23437d778eff20186ba575d62a9b
Alejandro Perez <alexbdn18@gmail.com>
2019-06-26 17:46:57 +0200

    first commit

---
+++ src/main/java/model/Town.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Town {
+
+	private Long idTown;
+	private String name;
+	
+	
+	public Long getIdTown() {
+		return idTown;
+	}
+	public void setIdTown(Long idTown) {
+		this.idTown = idTown;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
